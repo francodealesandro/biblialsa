1cc79d3cd7ada9a83fdba0b8822c22d9f9e0b2c3
TGVAD\fdealesandro <fdealesandro@ext.prevencionart.com.ar>
2026-01-04 18:41:23 -0300

    ghpages workflow

--- vite.config.js
+++ vite.config.js
@@ -35,9 +35,7 @@ export default defineConfig({
       },
       workbox: {
         globPatterns: ['**/*.{js,css,html,svg,json}']
-      },
-      srcDir: 'public',
-      filename: 'sw.js'
+      }
     })
   ],
 })
